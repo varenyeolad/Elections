79e175cf60052ec6a286991bc9613acc83bcf88b
Aizhan <97789571+varenyeolad@users.noreply.github.com>
2023-04-15 23:18:48 +0600

    update

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,31 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: '127.0.0.1',
+      host: "127.0.0.1",
       port: 7545,
-      network_id: '*', // Match any network id
+      network_id: "*",
     },
-    rinkeby: {
-      host: '127.0.0.1',
-      port: 8545,
-      network_id: 4,
-      gas: 4700000,
+    bscTestnet: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          "https://data-seed-prebsc-1-s1.binance.org:8545"
+        ),
+      network_id: 97, // Add network_id field here
+    },
+    bscMainnet: {
+      provider: () =>
+        new HDWalletProvider(mnemonic, "https://bsc-dataseed1.binance.org"),
+      network_id: 56,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "^0.5.0",
     },
   },
 };
